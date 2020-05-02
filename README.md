3e81e1819f0a416d60b281333ba03363d12acab0
palmytree <jeff@palmytree.com>
2020-05-02 15:59:50 -0500

    added word generators

---
+++ README.md
@@ -0,0 +1,59 @@
+# About
+The goal of this repo is to create a set functions that generate random datatypes students can use while learning the fundamentals of JavaScript. The functions must easy to use and must generate data that helps students further their understanding.
+
+## Function List
+- randNumFloor(max)
+    - Returns a random integer >= 0 and < max
+- randNumCeil(max)
+    - Returns a random integer > 0 and <= max
+- arrayGen(content, length)
+    - Returns an array
+    - content: What will be placed at each index of the returned array
+    - length: Number representing length of the returned array
+- randPhoneNum()
+    - Returns a string of random numbers in the following format:
+    '123-456-7890'
+- randSsn()
+    - Returns a string of random numbers in the following format:
+    '123-12-1234'
+- randPersonObj()
+    - Returns a single object in the following format:
+    ```
+    {
+        first: 'Estelle',
+        last: 'Jenkins',
+        birthday: { day: 30, month: 9, year: 1933 },
+        email: 'Estelle.Jenkins@email.com',
+        phone: '304-210-5924',
+        ssn: '126-95-1727'
+    }
+    ```
+    - Note: *These are randomly generated names and numbers based on the contained txt files. Any likeness to a real person is purely coincidental*
+- randShortWord()
+    - Returns a random word 1-4 characters long
+- randMedWord()
+    - Returns a random word 5-8 characters long
+- randLongWord()
+    - Returns a random word 9+ characters long
+- randWord()
+    - Returns a random word
+
+## Example Uses
+**Returns an array with 100 people objects**
+```JS
+arrayGen(randPersonObj(), 100)
+```
+
+**Returns an array with 100 random words**
+```JS
+arrayGen(randWord(), 100)
+```
+
+**Returns an array with between 1 and 100 random words 9+ characters long**
+```JS
+arrayGen(randLongWord(), randNumCeil(100))
+```
+
+## Resources
+Word array generators use the swear free word lists from first20hours awesome repo
+[google-10000-english](https://github.com/first20hours/google-10000-english)
--- generator.js
+++ generator.js
@@ -1,12 +1,19 @@
-// This function will return a random integer between 1 and max
-// Ex. randNumGenerator(100) will return a random number between 1 and 100
+// Initializing fs.ReadStream API.
+// Function dependents: randPersonObj, randShortWord, randMedWord, randLongWord, randWord
 
-const randNumGenerator = max => Math.floor(Math.random() * max)
+const fs = require('fs')
 
-// This function will return an array with the length and content passed in
-// Ex. arrayGenerator(randNumGenerator(10), 10) will return an array with a length of 10 filled with random numbers between 1 and 10
+// Returns a random integer >= 0 < max
 
-const arrayGenerator = (content, length) => {
+const randNumFloor = max => Math.floor(Math.random() * max)
+
+// Returns a random integer > 0 <= max
+
+const randNumCeil = max => Math.ceil(Math.random() * max) 
+
+// Returns an array with the length and content passed in
+
+const arrayGen = (content, length) => {
     let rArray = []
     for (let i = 0; i < length; i++) {
         rArray.push(content)
@@ -14,36 +21,86 @@ const arrayGenerator = (content, length) => {
     return rArray
 }
 
-//This function will return an arr of random people objects
+// Returns a random phone number string
 
-const ssnGenerator = () => {
+const randPhoneNum = () => {
     let arr = []
     for (let i = 0; i < 10; i++) {
-        arr.push(randNumGenerator(10))
+        arr.push(randNumFloor(10))
     }
     return `${arr[0]}${arr[1]}${arr[2]}-${arr[3]}${arr[4]}${arr[5]}-${arr[6]}${arr[7]}${arr[8]}${arr[9]}`
 }
 
+// Returns a random social security number LIKE string
 
-const fs = require('fs')
-const personObjectGenerator = () => {
+const randSsn = () => {
+    let arr = []
+    for (let i = 0; i < 9; i++) {
+        arr.push(randNumFloor(10))
+    }
+    return `${arr[0]}${arr[1]}${arr[2]}-${arr[3]}${arr[4]}-${arr[5]}${arr[6]}${arr[7]}${arr[8]}`
+}
+
+// Returns a random person object using the attached txt files for first/last name
+// firstnames.txt and lastnames.txt must be in the same directory as the .js file
+// Person Object Structure:
+// {
+//     first: 'Estelle',
+//     last: 'Jenkins',
+//     birthday: { day: 30, month: 9, year: 1933 },
+//     email: 'Estelle.Jenkins@email.com',
+//     phone: '304-210-5924',
+//     ssn: '126-95-1727'
+// }
+  
+const randPersonObj = () => {
+    
     let firstNamesArray = fs.readFileSync("firstnames.txt").toString().split("\r\n")
     let lastNamesArray = fs.readFileSync("lastnames.txt").toString().split("\r\n")
-    let first = firstNamesArray[randNumGenerator(2000)]
-    let last = lastNamesArray[randNumGenerator(1000)]
+    let first = firstNamesArray[randNumFloor(2000)]
+    let last = lastNamesArray[randNumFloor(1000)]
     return {
         first,
         last,
         birthday: {
-            day: Math.ceil(Math.random()*30),
-            month: Math.ceil(Math.random()*12),
-            year: Math.floor(Math.random()*100) + 1920
+            day: randNumCeil(30),
+            month: randNumCeil(12),
+            year: randNumFloor(100) + 1919
         },
-        ssn: ssnGenerator(),
-        email: `${first}.${last}@email.com`
+        email: `${first}.${last}@email.com`,
+        phone: randPhoneNum(),
+        ssn: randSsn(),
     }
 }
 
+// Returns a random word 1-4 characters long
 
+const randShortWord = () => {
+    let wordArr = fs.readFileSync("google-10000-english-usa-no-swears-short.txt").toString().split("\r\n")
+    return wordArr[randNumFloor(wordArr.length)]
+}
 
-console.log(personObjectGenerator())
+// Returns a random word 5-8 characters long
+
+const randMedWord = () => {
+    let wordArr = fs.readFileSync("google-10000-english-usa-no-swears-medium.txt").toString().split("\r\n")
+    return wordArr[randNumFloor(wordArr.length)]
+}
+
+// Returns a random word 9+ characters long
+
+const randLongWord = () => {
+    let wordArr = fs.readFileSync("google-10000-english-usa-no-swears-long.txt").toString().split("\r\n")
+    return wordArr[randNumFloor(wordArr.length)]
+}
+
+// Returns a rondom word
+
+const randWord = () => {
+    let shortWordArr = fs.readFileSync("google-10000-english-usa-no-swears-short.txt").toString().split("\n")
+    let medWordArr = fs.readFileSync("google-10000-english-usa-no-swears-medium.txt").toString().split("\n")
+    let longWordArr = fs.readFileSync("google-10000-english-usa-no-swears-long.txt").toString().split("\n")
+    let wordArr = [...shortWordArr, ...medWordArr, ...longWordArr]
+    return wordArr[randNumFloor(wordArr.length)]
+}
+
